eae8beca1f14ffe781d253f6ff0daf1efec4710a
linhh611 <linhkod97@gmail.com>
2019-06-15 10:56:43 +0700

    update

--- quyetde/app.js
+++ quyetde/app.js
@@ -6,10 +6,67 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.get('/',(req, res)=>{
-    res.sendFile(__dirname+'/views/home.html');
+    
+    const fileData= fs.readFileSync('question.txt', {encoding:'utf-8'});
+    let questionList=[];
+    if(fileData){
+        questionList=JSON.parse(fileData);
+
+    }
+    if(questionList.length==0){
+        res.send("Question bank empty");
+    }
+    else{
+        const randomQuestion = questionList[Math.floor(Math.random()*questionList.length)];
+        res.send(`
+        <h1>
+        ${randomQuestion.content}
+        </h1>
+        <form action="/vote" method="POST">
+        <input type="hidden" name="id" value="${randomQuestion.id}">
+        <button type="submit" value="no" name="vote">Sai</button>
+       <button type="submit" value="yes" name="vote">Đúng</button>
+
+        </form>
+        <button>Kết quả</button>
+        <button>Câu hỏi khác</button>
+        `);
+    }
 });
 app.get('/ask',(req, res)=>{
     res.sendFile(__dirname+'/views/ask.html');
+});
+app.get('/vote/:questionId/:vote', (req, res)=>{
+    console.log(req.params);
+    const questionId = req.params.questionId;
+    const fileData= fs.readFileSync('question.txt', {encoding:'utf-8'});
+    let questionList=[];
+    if(fileData){
+        questionList=JSON.parse(fileData);
+    }
+    questionList[questionId].yes+=1;
+    fs.writeFileSync('question.txt',JSON.stringify(questionList));
+    res.redirect('/');
+});
+
+app.post('/vote', (req,res)=>{
+    console.log(req.body);
+    const fileData= fs.readFileSync('question.txt', {encoding:'utf-8'});
+    let questionList=[];
+    const vote = req.body.vote;
+    const id = req.body.id;
+
+    
+    if(fileData){
+        questionList=JSON.parse(fileData);
+    }
+    if(vote == 'no'){
+        questionList[id].no+=1;
+    }else{
+        questionList[id].yes+=1;
+    }
+    fs.writeFileSync('question.txt',JSON.stringify(questionList));
+    res.redirect('/');
 });
 
 // app.get('/ask',(req, res)=>{
@@ -35,10 +92,7 @@ app.post('/addquestion',(req, res)=>{
 app.post('/addquestion',(req, res)=>{
     console.log(req.body);
 });
-app.get('/getquestion',(req,res)=>{
-    const fileData= fs.readFileSync('question.txt', {encoding:'utf-8'});
-    let question =[];
-});
+
 app.use('/public',express.static('public'));
 const port = 6661;
 app.listen(port, (err)=>{
